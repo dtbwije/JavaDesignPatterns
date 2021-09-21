37a790697b353d2ba9f0cea5406879066cf9751c
dtbwije <tharanga.wijethilake@pelion.com>
2021-09-22 00:28:48 +0300

    May be

--- FuntionalProgramming/src/Main.java
+++ FuntionalProgramming/src/Main.java
@@ -1,11 +1,26 @@
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class Main {
-    public static void main(String... a){
-        TriFunction<Integer,Integer,Integer,Integer> triFunction = (x,y,z) -> x+y+z;
 
-        System.out.println(triFunction.apply(1,2,4));
+    public static void main(String... a) {
+        TriFunction<Integer, Integer, Integer, Integer> triFunction = (x, y, z) -> x + y + z;
 
-        TriFunction<String, String,String, Integer> secondTriFunction = (x,y,z) -> x.length() + y.length() + z.length();
+        System.out.println(triFunction.apply(1, 2, 4));
+
+        TriFunction<String, String, String, Integer> secondTriFunction = (x, y, z) -> x.length() + y.length() + z.length();
 
         System.out.println(secondTriFunction.apply("Dayan", "Tharanga", "Bandara"));
+
+        BiFunction<Integer, Integer, Integer> devidFunction = (x, y) -> (x / y);
+
+        Function<BiFunction<Integer, Integer, Integer>, BiFunction<Integer, Integer, Integer>> testFunciton = func -> (x, y) -> {
+            if (y < 1) {
+                System.out.println("ERROR");
+            }
+            return func.apply(x,y);
+        };
+
+        BiFunction testFunctoinAsParms = testFunciton.apply(devidFunction.apply(3,4));
     }
 }
